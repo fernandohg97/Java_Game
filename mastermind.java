15ec0060c995b372a2078a49082ee7d9f713b13d
Fernando Hernandez <fernando.hdz97@gmail.com>
2016-12-16 22:49:03 -0800

    Se agrego documentacion

--- mastermind.java
+++ mastermind.java
@@ -1,3 +1,12 @@
+// Juego mastermind
+// Instrucciones:
+// El juego consiste en adivinar una cifra numerica, ya sea de 3, 4 o 5 numeros de acuerdo al nivel que elijas.
+// El numero a adivinar es dado aleatoriamente por el programa.
+// El mismo programa te ira dando ciertos aciertos y coincidencias del numero que ingresaste.
+/* Los aciertos indican que uno de los numeros que ingresaste estan en la misma posicion que el numero escondido,
+y las coincidencias indican que los numeros ingresados forman parte de la cifra pero en distinta posicion. */
+// Puede haber numeros repetidos y necesitaras ingresar un numero a la vez.
+
 import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
@@ -223,6 +232,7 @@ class mastermind
 
       System.out.println("ADIVINASTE!!!!!");
       System.out.println("Lo hiciste en "+attempt+" intentos");
+      System.out.println("Escribe 'si' si deseas seguir jugando o 'no' si deseas salir");
       System.out.print("Quieres volver a jugar: ");
       opcion = input.next();
       while (!opcion.equals("si") && !opcion.equals("no"))
